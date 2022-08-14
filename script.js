58ea18363b7cb93c9755f5609076b868d345ed65
jarenella <jamesa122333@gmail.com>
2022-08-14 12:04:33 -0700

    updated script.js code so that every else if statement contains an answer for what to do when the parameters are met. previously it was only the first few to test the code

--- script.js
+++ script.js
@@ -17,13 +17,6 @@ var defaultPlusUpperCase = defaultCharacters.concat(upperCaseYes);
 var defaultPlusSpecialCharacters = defaultCharacters.concat(specialCharactersYes);
 var allCharacters = defaultCharacters.concat(upperCaseYes, specialCharactersYes);
 
-
-
-//generator for all password options
-//for (i=0; i < 4; i++) {
-//  const randomCharacter = defaultCharacters[Math.floor(Math.random() * defaultCharacters.length)];
-//};
-
 // confirms special characters and caps then generates the password and writes it to the #password input
 function writePassword() {
 
@@ -34,8 +27,8 @@ function writePassword() {
 
   var characterCount = prompt("How many characters would you like to make your password? Please choose 8-10 characters.");
 
-  //determines what array to pick random characters from based on the answers to questions
-  //FALSE FALSE
+  //determines what array to pick random characters from based on the answers to questions. After characters are chosen at random, they are pushed into the afforementioned passwordArray variable
+  //FALSE FALSE (no special characters or caps)
   if (yesSpecialCharacters == false && yesCapitalLetters ==false && characterCount == 8) {
     for (i=0; i < 8; i++) {
       var randomCharacter = defaultCharacters[Math.floor(Math.random() * defaultCharacters.length)];
@@ -54,47 +47,74 @@ function writePassword() {
       passwordArray.push(randomCharacter);
     };
   }
-  //TRUE FALSE
+  //TRUE FALSE (special characters but no caps)
   else if (yesSpecialCharacters == true && yesCapitalLetters ==false && characterCount == 8) {
-    console.log("test2 character count 8");
+    for (i=0; i < 8; i++) {
+      var randomCharacter = defaultPlusSpecialCharacters[Math.floor(Math.random() * defaultPlusSpecialCharacters.length)];
+      passwordArray.push(randomCharacter);
+    };
   }
   else if (yesSpecialCharacters == true && yesCapitalLetters ==false && characterCount == 9) {
-    console.log("test2 character count 9");
+    for (i=0; i < 9; i++) {
+      var randomCharacter = defaultPlusSpecialCharacters[Math.floor(Math.random() * defaultPlusSpecialCharacters.length)];
+      passwordArray.push(randomCharacter);
+    };
   }
   else if (yesSpecialCharacters == true && yesCapitalLetters ==false && characterCount == 10) {
-    console.log("test2 character count 10");
+    for (i=0; i < 10; i++) {
+      var randomCharacter = defaultPlusSpecialCharacters[Math.floor(Math.random() * defaultPlusSpecialCharacters.length)];
+      passwordArray.push(randomCharacter);
+    };
   }
-  //TRUE TRUE
+  //TRUE TRUE (special characters and caps)
   else if (yesSpecialCharacters == true && yesCapitalLetters ==true && characterCount == 8) {
-    console.log("test3 character count 8");
+    for (i=0; i < 8; i++) {
+      var randomCharacter = allCharacters[Math.floor(Math.random() * allCharacters.length)];
+      passwordArray.push(randomCharacter);
+    };
   }
   else if (yesSpecialCharacters == true && yesCapitalLetters ==true && characterCount == 9) {
-    console.log("test3 character count 9");
+    for (i=0; i < 9; i++) {
+      var randomCharacter = allCharacters[Math.floor(Math.random() * allCharacters.length)];
+      passwordArray.push(randomCharacter);
+    };
   }
   else if (yesSpecialCharacters == true && yesCapitalLetters ==true && characterCount == 10) {
-    console.log("test3 character count 10");
+    for (i=0; i < 10; i++) {
+      var randomCharacter = allCharacters[Math.floor(Math.random() * allCharacters.length)];
+      passwordArray.push(randomCharacter);
+    };
   }
-  //FALSE TRUE
+  //FALSE TRUE (no special characters but still caps)
   else if (yesSpecialCharacters == false && yesCapitalLetters ==true && characterCount == 8) {
-    console.log("test4 character count 8");
+    for (i=0; i < 8; i++) {
+      var randomCharacter = defaultPlusUpperCase[Math.floor(Math.random() * defaultPlusUpperCase.length)];
+      passwordArray.push(randomCharacter);
+    };
   }
   else if (yesSpecialCharacters == false && yesCapitalLetters ==true && characterCount == 9) {
-    console.log("test4 character count 9");
+    for (i=0; i < 9; i++) {
+      var randomCharacter = defaultPlusUpperCase[Math.floor(Math.random() * defaultPlusUpperCase.length)];
+      passwordArray.push(randomCharacter);
+    };
   }
   else if (yesSpecialCharacters == false && yesCapitalLetters ==true && characterCount == 10) {
-    console.log("test4 character count 10");
+    for (i=0; i < 10; i++) {
+      var randomCharacter = defaultPlusUpperCase[Math.floor(Math.random() * defaultPlusUpperCase.length)];
+      passwordArray.push(randomCharacter);
+    };
   }
-  //if the character count is too small, a message will pop up, and pressing ok will restart the questioning process
+  //if the character count is too small or too large, a message will pop up, and pressing ok will restart the questioning process
   else if (characterCount < 8 || characterCount > 10) {
     confirm("Please enter a password length between 8 to 10 characters.");
     writePassword();
   }
   //else statement just incase
   else {
-    alert("Something went wrong! Please refresh the page and try again. If this message popped up, then the coder did something very wrong because it shouldn't pop up.");
+    alert("Something went wrong! Please refresh the page and try again. Remember to choose a password length between 8 to 10 characters and click OK to confirm. For the yes or no questions, treat ok as yes and cancel as no");
   }
 
-  //turns the password array into a string
+  //turns the password array (named passwordArray) into a string
   let passwordWithCommas = passwordArray.toString();
   //removes the commas from the password string
   let password = passwordWithCommas.replaceAll(',','');
